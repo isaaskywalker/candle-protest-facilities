5e33a353d05090e272a47017e8f3f4191fd2eadf
Isabelle <belleyuri@gmail.com>
2025-01-05 10:53:39 +0900

    Update Marker.tsx

---
+++ src/components/Map/Marker.tsx
@@ -0,0 +1,33 @@
+# src/components/Map/Marker.tsx
+'use client'
+
+import { Marker } from '@react-google-maps/api'
+import type { Facility } from '@/types'
+
+interface MarkerProps {
+  facility: Facility;
+  onSelect: () => void;
+}
+
+const markerColors = {
+  '화장실': 'red',
+  '음식': 'green',
+  '지하철': 'blue'
+} as const
+
+export default function CustomMarker({ facility, onSelect }: MarkerProps) {
+  return (
+    <Marker
+      position={facility.location}
+      onClick={onSelect}
+      icon={{
+        path: google.maps.SymbolPath.CIRCLE,
+        fillColor: markerColors[facility.category],
+        fillOpacity: 0.7,
+        strokeWeight: 2,
+        scale: 10
+      }}
+      title={facility.name}
+    />
+  )
+}
