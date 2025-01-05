210c8ffc2a6c16fc826c3addea08307925ebcf37
Isabelle <belleyuri@gmail.com>
2025-01-05 14:11:01 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['maps.googleapis.com'],
-    },
-    env: {
-      NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    },
-    webpack: (config) => {
-      config.resolve.fallback = { fs: false, net: false, tls: false };
-      return config;
-    },
-  }
-  
-  module.exports = nextConfig
-  
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['maps.googleapis.com'],
+  },
+  webpack: (config, { isServer }) => {
+    // 클라이언트 사이드에서만 적용
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        stream: false,
+        util: false,
+        url: false,
+        zlib: false,
+        http: false,
+        https: false,
+        assert: false,
+        os: false,
+      }
+    }
+    return config
+  },
+  transpilePackages: ['@firebase/auth']
+}
+
+module.exports = nextConfig
